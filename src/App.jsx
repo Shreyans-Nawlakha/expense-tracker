e362aaca1585ce80eb33da7e67355c5271a0a64e
Shreyans-Nawlakha <86418929+Shreyans-Nawlakha@users.noreply.github.com>
2024-04-10 20:33:19 +0530

    background changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ExpenseTracker from "./components/ExpenseTracker"
 function App() {
   return (
     <>
-      <div className="container max-h-screen"><ExpenseTracker/></div>
+      <div className="container max-h-screen bg-bgd"><ExpenseTracker/></div>
     </>
   )
 }
